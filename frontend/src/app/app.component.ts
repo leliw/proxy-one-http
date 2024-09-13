deac5a2665cf64c21be65c9f1adf4a4ac1238a13
Marcin Leliwa <marcin.leliwa@saltus.pl>
2024-09-13 08:53:09 +0200

    Utworzenie komponentu nawigacji

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { ConfigService } from './core/config.service';
 import { StatusViewComponent } from './proxy/status-view/status-view.component';
+import { NavComponent } from "./core/nav/nav.component";
 
 export interface Hello {
     Hello: string;
@@ -11,7 +12,7 @@ export interface Hello {
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [CommonModule, RouterOutlet, StatusViewComponent],
+    imports: [CommonModule, RouterOutlet, StatusViewComponent, NavComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
@@ -20,7 +21,7 @@ export class AppComponent {
 
     version = '';
 
-    constructor(private http: HttpClient, private config: ConfigService) {
+    constructor(private config: ConfigService) {
         this.config.getConfig().subscribe(c => {
             this.version = c.version;
         })

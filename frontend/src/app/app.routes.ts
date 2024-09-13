b72cac19068e58d0566ddf6d2910d73ac7a52ed0
Marcin Leliwa <marcin.leliwa@saltus.pl>
2024-09-13 10:27:07 +0200

    Wygenerowanie standardowego komponentu, dodanie routingu i nawigacji

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'sessions', data: { title: "Zarejestrowane sesje HTTP"},
+        loadComponent: () => import('./features/sessions/session-table/session-table.component').then(mod => mod.SessionTableComponent)
+    },
+];

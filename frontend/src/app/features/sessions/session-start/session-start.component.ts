6e5b9e07175ebcb4f711cff0ffafbf16a899cb80
Marcin Leliwa <marcin.leliwa@saltus.pl>
2024-09-13 12:10:38 +0200

    SessionStartComponent - default content

--- frontend/src/app/features/sessions/session-start/session-start.component.ts
+++ frontend/src/app/features/sessions/session-start/session-start.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { SessionStartFromComponent } from "../session-start-from/session-start-from.component";
 
 @Component({
-  selector: 'app-session-start',
-  standalone: true,
-  imports: [],
-  templateUrl: './session-start.component.html',
-  styleUrl: './session-start.component.css'
+    selector: 'app-session-start',
+    standalone: true,
+    imports: [
+    MatCardModule,
+    MatProgressBarModule,
+    MatButtonModule,
+],
+    templateUrl: './session-start.component.html',
+    styleUrl: './session-start.component.css'
 })
 export class SessionStartComponent {
+    status = signal("READY");
+    percent = signal(0);
 
+    start(): void {
+        this.status.set('IN PROGRESS');
+    }
+
+    stop(): void {
+        this.status.set('CANCELED');
+    }
 }
